690b2eb3c01bbb6a67232058ec197c23b33d8691
Yamila Holguin <yamila.holguin@ister.edu.ec>
2026-05-29 17:12:19 -0500

    cambios de diseño

--- app/src/main/java/com/example/calculadoraimc/Pantallas.kt
+++ app/src/main/java/com/example/calculadoraimc/Pantallas.kt
@@ -140,7 +140,7 @@ fun PantallaResultado(
         }
         imc < 30.0 -> {
             categoria = "Sobrepeso"
-            colorTexto = Color.Yellow
+            colorTexto = Color.Blue
         }
         else -> {
             categoria = "Obesidad"
